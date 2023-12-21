19ad2161f98688125ccd46ae5fdbd03667d680c6
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-21 16:48:32 +0900

    remove executable code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
 class Card {}
-
-// 실제 실행코드
-const cards = new Cards(3);
-document.querySelector("#contents").appendChild(cards.deck[0].node);
-document.querySelector("#contents").appendChild(cards.deck[1].node);
-document.querySelector("#contents").appendChild(cards.deck[2].node);
