29581048d33fdbef6a255ddee22c06c68e933efa
a-honey <jungahyeon0512@khu.ac.kr>
2023-12-22 00:19:55 +0900

    1단계: 카드 뒤집기 게임 제출

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-class Card {}
+class Card {
+  constructor(isWinningCard) {
+    this.node = document.createElement("button");
+    this.node.style.height = "200px";
+    this.node.style.width = "100px";
+    this.node.innerText = "두근두근";
+
+    this.isWinningCard = isWinningCard;
+
+    this.handleCardClick();
+  }
+
+  selectCard() {
+    if (this.isWinningCard) {
+      document.querySelector("#contents").innerText = "당첨입니다 :D";
+    }
+    document.querySelector("#contents").innerText = "꽝입니다!";
+  }
+
+  handleCardClick() {
+    this.node.addEventListener("click", () => {
+      this.selectCard();
+    });
+  }
+}
+
+class Cards {
+  constructor(count) {
+    this.createCards(count);
+    this.shuffle();
+  }
+
+  createCards(count) {
+    if (count < 2) alert("하나 이상의 카드를 입력해주세요.");
+
+    this.cardList = new Array(count - 1).fill(0).map(() => new Card(false));
+    this.cardList.push(new Card(true));
+  }
+  shuffle() {
+    this.cardList.sort(() => Math.random() - 0.5);
+  }
+}
+
+const cards = new Cards(5);
+
+cards.cardList.forEach((card) => {
+  document.querySelector("#contents").appendChild(card.node);
+});
